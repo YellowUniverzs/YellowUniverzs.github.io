e4aa9bc81604bb9713fb62b37ab7c16d2d884db9
YellowUniverzs <yellowuniverz@gmail.com>
2026-01-11 21:20:48 +0700

    update article dan image terbaru

--- project/js/article/korea.js
+++ project/js/article/korea.js
@@ -1,15 +1,87 @@
 /* Data artikel */
    export const koreaArticles = [
 
-{ 
-      
+{
+      title: "Shin Min Ah & Lee Jin Wook | Wed Thurs Fri", 
+      date: "2026-01-09",
+      desc: "Shin Min Ah & Lee Jin Wook Dikabarkan Akan Bintangi Drama Romance Terbaru | Wed Thurs Fri", 
+      img: "project/picture/2026/korea/wed-thurs-fri/thumbnail/shin-min-ah-x-lee-jin-wook.jpg", 
+      rec: false,
+      link: "project/article/2026/korea/wed-thurs-fri/artikel1/index.html"
+    },
+{
+      title: "Hwang In Yeop | Human X Gumiho", 
+      date: "2026-01-08",
+      desc: "Hwang In Yeop Dikabarkan Bergabung dengan Ji Chang Wook dan Jun Ji Hyun Bintangi Drama Terbaru | Human X Gumiho", 
+      img: "project/picture/2026/korea/human-x-gumiho/thumbnail/hwang-in-yeop.jpg", 
+      rec: true,
+      link: "project/article/2026/korea/human-x-gumiho/artikel1/index.html"
+    },
+
+  {
+      title: "Hani EXID | Love is Coming", 
+      date: "2026-01-08",
+      desc: "Hani EXID Sedang Dalam Pembicaraan untuk Bintangi Drama Akhir Pekan Terbaru | Love is Coming", 
+      img: "project/picture/2026/korea/love-is-coming/thumbnail/hani-exid.jpg", 
+      rec: false,
+      link: "project/article/2026/korea/love-is-coming/artikel1/index.html"
+    },
+   {
+      title: "Efficident Dating for Singles", 
+      date: "2026-01-08",
+      desc: "JTBC Bagikan Pembacaan Naskah untuk Drama Terbaru | Efficident Dating for Singles", 
+      img: "project/picture/2026/korea/efficient-dating-for-singles/thumbnail/efficient-dating-for-singles.jpg", 
+      rec: true,
+      link: "project/article/2026/korea/efficient-dating-for-singles/artikel1/index.html"
+    },
+   {
+      title: "Beauty in the Beast", 
+      date: "2026-01-08",
+      desc: "Netflix Bagikan Pembacaan Naskah Para Pemeran untuk Drama Terbaru | Beauty and the Beast", 
+      img: "project/picture/2026/korea/beauty-in-the-beast/thumbnail/beauty-in-the-beast.jpg", 
+      rec: false,
+      link: "project/article/2026/korea/beauty-in-the-beast/artikel1/index.html"
+    },
+    {
+      title: "Park Min Young & Yook Sungjae BTOB | Nine to Six", 
+      date: "2026-01-06",
+      desc: "Park Min Young & Yook Sungjae BTOB dalam Pembicaraan untuk Membintangi Drama Romance Terbaru | Nine to Six", 
+      img: "project/picture/2026/korea/nine-to-six/thumbnail/park-min-young-x-yook-sungjae.jpg", 
+      rec: true,
+      link: "project/article/2026/korea/nine-to-six/artikel1/index.html"
+    },
+   {
+      title: "Joo Won & Park Bo Gum | The Sword: a Legend of Red Wolf", 
+      date: "2026-01-06",
+      desc: "Joo Won dalam Pembicaraan untuk Bintangi Film Action Historical Terbaru | The Sword: a Legend of Red Wolf", 
+      img: "project/picture/2026/korea/the-sword-a-legend-of-red-wolf/thumbnail/joo-won-x-park-bo-gum.jpg", 
+      rec: false,
+      link: "project/article/2026/korea/the-sword-a-legend-of-red-wolf/artikel1/index.html"
+    },
+    {
+      title: "Park Jin Hee & Nam Sang Ji | Red Pearl", 
+      date: "2026-01-05",
+      desc: "Park Jin Hee & Nam Sang Ji Akan Membintangi Drama Harian Terbaru | Red Pearl", 
+      img: "project/picture/2026/korea/red-pearl/thumbnail/park-jin-hee-x-nam-sang-ji.jpg", 
+      rec: true,
+      link: "project/article/2026/korea/red-pearl/artikel1/index.html"
+    },
+    {
+      title: "Song Joong Ki & Park Ji Hyun", 
+      date: "2026-01-05",
+      desc: "Song Joong Ki & Park Ji Hyun Sedang dalam Pembicaraan untuk Bintangi Drama Romance Comedy Terbaru | Love Cloud", 
+      img: "project/picture/2026/korea/love-cloud/thumbnail/song-joong-ki-x-park-jin-hyun.jpg", 
+      rec: false,
+      link: "project/article/2026/korea/love-cloud/artikel1/index.html"
+    },
+    {
       title: "K-Drama Terbaru Bulan Januari 2026", 
       date: "2026-01-05",
       desc: "K-Drama Series Terbaru Bulan Januari 2026", 
       img: "project/picture/2026/korea/2026/januari/thumbnail/drakor-terbaru-januari.jpg", 
-      rec: false,
+      rec: true,
       link: "project/article/2026/korea/2026/januari/artikel1/index.html"
-      },
+    },
   ];
 
 
