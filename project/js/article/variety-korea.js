527a74f4d31336e0efca8a0c4b77bb8f6d759e7c
YellowUniverzs <yellowuniverz@gmail.com>
2026-01-14 04:57:08 +0700

    update article dan image terbaru

--- project/js/article/variety-korea.js
+++ project/js/article/variety-korea.js
@@ -1,7 +1,20 @@
 /* Data artikel */
 export const varietyKoreaArticles = [
-     { 
-      
+     {
+      title: "Crime Scene Zero 6", 
+      date: "2026-01-13",
+      desc: "Netflix Konfirmasi Game Show | Crime Scene Zero | Akan Diproduksi Season ke-6", 
+      img: "project/picture/2026/variety-korea/crime-scene-zero/thumbnail/crime-scene-zero.jpg", 
+      rec: true,
+      link: "project/article/2026/variety-korea/crime-scene-zero/artikel1/index.html"
+     },
+     {
+      title: "The Secret Friends Club", 
+      date: "2026-01-13",
+      desc: "MBC Bagikan Poster Pertama Veriety Show Terbaru | The Secret Friends Club", 
+      img: "project/picture/2026/variety-korea/the-secret-friends-club/thumbnail/the-secret-friends-club.jpg", 
+      rec: false,
+      link: "project/article/2026/variety-korea/the-secret-friends-club/artikel1/index.html"
      },
   ];
 
