a2b950eb506be257152fa83cae371bcc5c77fa3b
YellowUniverzs <yellowuniverz@gmail.com>
2026-01-14 15:20:47 +0700

    update article dan image terbaru

--- project/js/article/thailand.js
+++ project/js/article/thailand.js
@@ -1,7 +1,12 @@
 /* Data artikel */
 export const thailandArticles = [
-    { 
-      
+    {
+      title: "Girl From Nowhere: The Reset", 
+      date: "2026-01-14",
+      desc: "Netflix Merilis Official Poster untuk Serial Terbaru | Girl From Nowhere: The Reset", 
+      img: "project/picture/2026/thailand/girl-from-nowhere-the-reset/thumbnail/girl-from-nowhere-the-reset.jpg", 
+      rec: false,
+      link: "project/article/2026/thailand/girl-from-nowhere-the-reset/artikel1/index.html"
     },
   ];
 
